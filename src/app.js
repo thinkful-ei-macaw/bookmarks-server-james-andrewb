637566c133beddf778c40d2a87f242797eaa9559
Andrew Burchett <andrew.t.w.burchett@gmail.com>
2020-03-27 11:36:38 -0700

    config logging and API key handling middleware

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
 
 const app = express();
 
